d374b923aca7fb74796f82fcf25f44b601e4413d
Fabio Soares <fabiosoares@outlook.com.br>
2019-05-07 21:34:53 -0300

    Add methods to send info to stores

--- src/actions/restaurant.js
+++ src/actions/restaurant.js
@@ -1,5 +1,7 @@
 import restaurants from "../reducers/restaurants";
+import { LOAD_RESTAURANTS } from "../actions/action_types";
+
 export const loadRestaurants = () => ({
-  type: 'LOAD_RESTAURANTS',
+  type: LOAD_RESTAURANTS,
   restaurants: restaurants
 });
